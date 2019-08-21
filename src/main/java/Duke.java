c70b89c2b9519ff1be5fd05fc1f1ed39f5d5161f
x3chillax <blackshot4life@hotmail.com>
2019-08-21 16:26:18 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,18 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke
 {
     public static void main(String[] args)
     {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        */
         String line = "     ____________________________________________________________\n";
         System.out.println(line + "     Hello! I'm Duke\n" + "     What can I do for you?\n" + line);
         Scanner input = new Scanner(System.in);
+        ArrayList<String> arlist = new ArrayList<String>();
+        int i = 0;
         while(true)
         {
-            String command = input.next();
+            String command = input.nextLine();
             if(command.equals("bye"))
             {
                 System.out.print(line);
@@ -25,9 +20,31 @@ public class Duke
                 System.out.println(line);
                 break;
             }
-            System.out.print(line);
-            System.out.println("     " + command);
-            System.out.println(line);
+            if(command.equals("list"))
+            {
+                System.out.print(line);
+                for(int j = 0; j < i; j += 1)
+                {
+                    System.out.println("      " +  (j+1) + ". " + arlist.get(j));
+                }
+                System.out.println(line);
+            }
+            else
+            {
+                System.out.print(line);
+                System.out.println("     " + command);
+                System.out.println(line);
+                arlist.add(i, command);
+                i += 1;
+            }
+            /*
+            else
+            {
+                System.out.print(line);
+                System.out.println("     " + command);
+                System.out.println(line);
+            }
+            */
         }
     }
 }
