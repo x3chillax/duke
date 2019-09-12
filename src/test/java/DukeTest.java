909efca6f3f0eed2b635653b3cab88a9135de23b
x3chillax <blackshot4life@hotmail.com>
2019-09-12 14:35:37 +0800

    A-JUnit

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,10 +1,25 @@
+
 import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class DukeTest
 {
     @Test
     public void dummyTest()
     {
-        assertEquals(2, 2);
+        assertEquals(2,2);
     }
+    @Test
+    public void TestIcon()
+    {
+        try
+        {
+            assertEquals("✘", new Deadline("testing", "20/12/2013").getStatusIcon());
+        } catch (DukeException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
